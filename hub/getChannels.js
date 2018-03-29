b64ddbf28bdea2e6dbfb11cd53613506a2c669a7
Rahul <rksethuram9@gmail.com>
2018-03-29 16:59:34 -0700

    Get channels

---
+++ hub/getChannels.js
@@ -0,0 +1,27 @@
+const { asyncRequest } = require('../util')
+const { query, validationResult } = require('express-validator/check')
+const { matchedData } = require('express-validator/filter')
+const { getModels } = require('../models')
+const { Op } = require('sequelize')
+
+const validator = [query('address', 'Please provide address.').exists()]
+
+const handler = async (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.mapped() })
+  }
+
+  const { address } = matchedData(req)
+
+  const { Channel } = getModels()
+  const channels = await Channel.findAll({
+    where: {
+      [Op.or]: [{ agentA: address }, { agentB: address }]
+    }
+  })
+  res.status(200).json({ channels })
+}
+
+module.exports.validator = validator
+module.exports.handler = asyncRequest.bind(null, handler)
