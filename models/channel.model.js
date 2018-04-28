b84ebd28ad0adba2db10586062f42852ebd970a0
Rahul <rksethuram9@gmail.com>
2018-04-27 18:24:27 -0700

    Token contract

--- models/channel.model.js
+++ models/channel.model.js
@@ -7,6 +7,10 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
         primaryKey: true
       },
+      tokenContract: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      },
       depositA: {
         type: DataTypes.TEXT,
         allowNull: false
