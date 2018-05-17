28b6b270805bddc65e1b66f553b844cba85a9b98
LayneHaber <layne.haber@gmail.com>
2018-05-16 18:39:08 -0700

    create vc transaction model

---
+++ models/virtualtransaction.model.js
@@ -0,0 +1,49 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define(
+    'virtualtransactions',
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false
+      },
+      virtualchannelId: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      hash: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      sigA: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      sigB: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      balanceA: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      balanceB: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      nonce: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      currentState: {
+        type: DataTypes.STRING,
+        allowNull: true
+      }
+    },
+    {
+      tableName: 'virtualtransactions',
+      timestamps: true
+    }
+  )
+}
