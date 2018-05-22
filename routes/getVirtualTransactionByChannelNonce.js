5d99cc2aa1e07845bea3757afd9a4f9b8df186c9
LayneHaber <layne.haber@gmail.com>
2018-05-21 18:05:06 -0700

    fix virtual tx getter

--- routes/getVirtualTransactionByChannelNonce.js
+++ routes/getVirtualTransactionByChannelNonce.js
@@ -16,12 +16,21 @@ const handler = async (req, res, next) => {
   const { channel, nonce } = matchedData(req)
 
   const { VirtualTransaction } = getModels()
-  const transaction = await VirtualTransaction.findOne({
-    where: {
-      virtualchannelId: channel.toLowerCase(),
-      nonce
-    }
-  })
+  let transaction
+  if (nonce !== '0') {
+    transaction = await VirtualTransaction.findOne({
+      where: {
+        channelId: channel.toLowerCase(),
+        nonce
+      }
+    })
+  } else {
+    transaction = await VirtualTransaction.findAll({
+      where: {
+        channelId: channel.toLowerCase()
+      }
+    })
+  }
 
   if (!transaction) {
     res.status(404).json({ error: 'Could not find transaction.' })
