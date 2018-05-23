a793c313787322508ce5af1bf0a8c5ae51caf2ab
LayneHaber <layne.haber@gmail.com>
2018-05-23 14:39:03 -0700

    fix join vc

--- routes/joinVirtualChannel.js
+++ routes/joinVirtualChannel.js
@@ -25,20 +25,30 @@ const handler = async (req, res, next) => {
       message: 'Virtual Channel not found'
     })
   }
-  if (vc.status !== 'opening') {
+  if (vc.status.toLowerCase() !== 'opening') {
     return res.status(400).json({
       message: 'Channel cannot be joined at this time.'
     })
   }
-  const { agentA, agentB, ingrid } = vc
+  const {
+    agentA,
+    agentB,
+    ingrid,
+    subchanAtoI,
+    subchanBtoI,
+    closingTimeSeconds
+  } = vc
 
   let signer = Ethcalate.recoverSignerFromOpeningCerts(cert, {
     id,
     agentA,
     agentB,
     ingrid,
+    subchanAtoI,
+    subchanBtoI,
     participantType: 'agentB',
-    depositInWei: depositB
+    depositInWei: depositB,
+    closingTimeSeconds
   })
 
   if (signer.toLowerCase() !== agentB) {
